7132ed77daa32db1de00b1a8bcc11926850aa57a
Rodrigo López Dato <rodrigo@auth0.com>
2015-04-22 16:14:02 -0300

    Add APIv2 convenience methods for Auth0User
    Also, Auth0User.get was made public in case there's a case we
    haven't considered. If it's private, there's no workaround without
    resorting to reflection or hacks.

--- src/main/java/com/auth0/Auth0User.java
+++ src/main/java/com/auth0/Auth0User.java
@@ -9,53 +9,69 @@ import us.monoid.json.JSONException;
 import us.monoid.json.JSONObject;
 
 public class Auth0User implements Principal {
-	
+
 	private JSONObject json;
 
 	public Auth0User(JSONObject json) {
 		this.json = json;
 	}
-	
+
 	public static Auth0User get(HttpServletRequest req) {
 		return (Auth0User) req.getSession().getAttribute("user");
 	}
-	
+
 	public String getProperty(String prop) {
 		return get(prop, String.class);
 	}
-	
-	private <T> T get(String prop, Class<T> clazz) {
+
+	public <T> T get(String prop, Class<T> clazz) {
 		try {
 			return (T) json.get(prop);
 		} catch(JSONException ex) {
 			throw new IllegalStateException("Cannot get property " + prop + " from Auth0user", ex);
 		}
 	}
-	
+
+	public JSONObject getObject(String prop) {
+		return get(prop, JSONObject.class);
+	}
+
+	public JSONArray getArray(String prop) {
+		return get(prop, JSONArray.class);
+	}
+
+	public JSONObject getUserMetadata() {
+		return getObject("user_metadata");
+	}
+
+	public JSONObject getAppMetadata() {
+		return getObject("app_metadata");
+	}
+
 	public String getName() {
 		return getProperty("name");
 	}
-	
+
 	public String getEmail() {
 		return getProperty("email");
 	}
-	
+
 	public String getUserId() {
 		return getProperty("user_id");
 	}
-	
+
 	public String getNickname() {
 		return getProperty("nickname");
 	}
-	
+
 	public String getPicture() {
 		return getProperty("picture");
 	}
-	
+
 	public JSONArray getIdentities() {
-		return get("identities", JSONArray.class);
+		return getArray("identities");
 	}
-	
-	
+
+
 
 }
